3d6da8d6ee4bd0933fbecfb667bdf3c512650630
Andy Lin <andy.lin@cellopoint.com>
2015-04-16 07:59:20 +0800

    sync

--- main.py
+++ main.py
@@ -57,6 +57,49 @@ class KanCello:
                 print "桶:%d".decode("utf-8","ignore") % material["api_value"]
             elif material["api_id"] == 7:
                 print "開發資才:%d".decode("utf-8","ignore") % material["api_value"]
+    def __getQuestList(self):
+        path = "/kcsapi/api_get_member/questlist"
+        payload = {
+        "api_token":self.api_token
+        "api_page_no":1,
+        "api_verno":1,
+        }
+        res = requests.post("http://{0}{1}".format(self.server,path),headers=self.header,data=payload).text
+        res = json.loads(re.findall("svdata=(.+)",res)[0])
+        if res["api_result"] == 1:
+            res = res["api_data"]
+            print "未完成任務總數:%d".encode("utf-8","ignore") % res["apt_count"]
+            for i in range(1,(res["api_page_count"]+1)):
+                payload["api_page_no"] = i
+                missions = requests.post("http://{0}{1}".format(self.server,path),headers=self.header,data=payload).text
+                missions = json.loads(re.findall("svdata=(.+)",missions)[0])
+                if missions["api_result"] == 1:
+                    missions = missions["api_data"]["api_list"]
+                    for mission in missions:
+                        print "任務ID:%d" % mission["api_no"]
+                        print "任務名稱:%s".encode("utf-8","ignore") % mission["api_title"]
+                        print "任務內容:%s".encode("utf-8","ignore") % mission["api_detail"]
+                        if mission["api_state"] == 2:
+                            print "任務接取".encode("utf-8","ignore")
+                        elif mission["api_state"] == 3:
+                            print "任務完成".encode("utf-8","ignore")
+                else:
+                    print "連線失敗".encode("utf-8","ignore")
+        else:
+            print "連線失敗".encode("utf-8","ignore")
+
+
+    def __getDeck(self):
+        path = "/kcsapi/api_get_member/deck"
+        payload = {
+        "api_token":self.api_token,
+        "api_verno":1
+        }
+        res = requests.post("http://{0}{1}".format(self.server,path),headers=self.header,data=payload).text
+        res = json.loads(re.findall("svdata=(.+)",res)[0])
+        return res
+    def __
     def __mainProcess(self):
         self.__getMaterial()
+
 KanCello()
